efc8ba55669ce1513bbd7be489d2583864fb623d
Iván Camacho <ivancabo19@gmail.com>
2025-07-18 09:31:01 -0600

    Ajustes finales para diseño responsivo y botones

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-
-document.getElementById("btn-copiar").addEventListener("click", function () {
-  const correo = "ivancabo19@gmail.com";
-  navigator.clipboard.writeText(correo).then(() => {
-    alert("Correo copiado al portapapeles.");
-  });
-});
-
 // Copiar correo
 document.getElementById("btn-copiar").addEventListener("click", () => {
   const correo = "ivancabo19@gmail.com";
@@ -20,7 +12,7 @@ document.getElementById("btn-mailto").addEventListener("click", () => {
 });
 
 // EmailJS: inicializar (pon tu public key)
-emailjs.init("VcvKmshr5HEVQASYC");
+emailjs.init("VcvKmshr5HEVQASYC"); // Asegúrate de que esta es tu Public Key correcta
 
 // Envío de formulario
 document
@@ -28,7 +20,7 @@ document
   .addEventListener("submit", function (e) {
     e.preventDefault();
     emailjs
-      .sendForm("service_ury4xzp", "template_k9f9iyf", this, "VcvKmshr5HEVQASYC")
+      .sendForm("service_ury4xzp", "template_k9f9iyf", this, "VcvKmshr5HEVQASYC") // Revisa que el service_ID, template_ID y Public Key sean correctos
       .then(() => {
         document.getElementById("mensaje-enviado").classList.remove("oculto");
         this.reset();
@@ -39,13 +31,9 @@ document
       });
   });
 
-  document.querySelector('.menu-toggle').addEventListener('click', () => {
-  document.querySelector('.navbar').classList.toggle('open');
-});
-
 // Menú hamburguesa responsive
 const menuToggle = document.querySelector('.menu-toggle');
-const navbar    = document.querySelector('.navbar');
+const navbar   = document.querySelector('.navbar');
 menuToggle.addEventListener('click', () => {
   const isOpen = navbar.classList.toggle('open');
   menuToggle.setAttribute('aria-expanded', isOpen);
